406c81ab245c642ff40fc41c238808578bafa668
vishal modanwal <vk076394@gmail.com>
2026-03-13 06:21:22 -0400

    create a custom component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EmployeeCardComponent } from './employee-card/employee-card.component';
 import { SvayamComponent } from './svayam/svayam.component';
+import { VishalComponent } from './vishal/vishal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeCardComponent,
-    SvayamComponent
+    SvayamComponent,
+    VishalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/vishal/vishal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : "app-vishal",
+    templateUrl : "./vishal.component.html",
+    styleUrls : ["./vishal.component.css"]
+})
+
+export class VishalComponent{
+    uName : string = "Vishal";
+
+}
